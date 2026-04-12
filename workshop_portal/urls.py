19b15aaf134896472d549de4a966526a9efc0ee0
arshraj1020 <arshraj1020@gmail.com>
2026-04-12 19:34:44 +0530

    Updated urls for Django 5 compatibility

--- workshop_portal/urls.py
+++ workshop_portal/urls.py
@@ -1,25 +1,24 @@
-from django.conf.urls import url, include
-from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
 from workshop_portal import views
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
     # Home
-    url(r'^$', views.index),
+    path('', views.index),
 
-    # ✅ FIXED (namespace added)
-    # support BOTH namespaces
-url(r'^workshop/', include(('workshop_app.urls', 'workshop_app'), namespace='workshop')),
+    # Workshop app
+    path('workshop/', include(('workshop_app.urls', 'workshop_app'), namespace='workshop')),
 
     # Django auth (password reset etc.)
-    url(r'^reset/', include('django.contrib.auth.urls')),
+    path('reset/', include('django.contrib.auth.urls')),
 
     # Other apps
-    url(r'^page/', include(('cms.urls', 'cms'), namespace='cms')),
-    url(r'^statistics/', include(('statistics_app.urls', 'statistics_app'), namespace='statistics_app')),
+    path('page/', include(('cms.urls', 'cms'), namespace='cms')),
+    path('statistics/', include(('statistics_app.urls', 'statistics_app'), namespace='statistics_app')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
